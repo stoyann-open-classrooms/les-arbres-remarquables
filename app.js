ca6bdba1e78e4e4efd612caff5cd0bfba997db0e
Stoyann <stoyann.openclassrooms@gmail.com>
2021-05-29 04:03:53 +1100

    naviguation

---
+++ app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".liste-nav");
+const btnMenu = document.querySelector(".btn-toggle-container");
+
+btnMenu.addEventListener("click", function () {
+  menu.classList.toggle("active-menu");
+});
