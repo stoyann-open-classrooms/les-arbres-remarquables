d114baf48848ff25492b4e9679122d81e317f8ab
Stoyann <stoyann.openclassrooms@gmail.com>
2021-05-30 15:45:15 +1100

    mise en place de sass

--- nav.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".liste-nav");
-const btnMenu = document.querySelector(".btn-toggle-container");
-
-// navigation
-
-btnMenu.addEventListener("click", function () {
-  menu.classList.toggle("active-menu");
-});
