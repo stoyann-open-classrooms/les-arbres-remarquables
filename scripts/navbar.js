ec56a837cd797c41e63cd9fd68635f24ecda180b
Stoyann <stoyann.openclassrooms@gmail.com>
2021-05-30 21:11:23 +1100

    'accueil-css'

---
+++ scripts/navbar.js
@@ -0,0 +1,19 @@
+const btnNav = document.querySelector(".responsive-nav-btn");
+
+const listeNav = document.querySelector("nav ul");
+
+let imgBtn = document.querySelector(".responsive-nav-btn img");
+
+imgBtn.addEventListener("click", () => {
+  listeNav.classList.toggle("active-nav");
+
+  if (imgBtn.src.includes("menu")) {
+    imgBtn.src = "../sources/svg/croix.svg";
+    imgBtn.getElementsByClassName.width = "30px";
+    imgBtn.getElementsByClassName.height = "30px";
+  } else {
+    imgBtn.src = "../sources/svg/menu.svg";
+    imgBtn.getElementsByClassName.width = "40px";
+    imgBtn.getElementsByClassName.height = "40px";
+  }
+});
