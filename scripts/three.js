cb5d8814b3788af6bb0e3dfebed0d14c7ca06cd7
Stoyann <stoyann.openclassrooms@gmail.com>
2021-06-02 15:53:58 +1100

    recuperer la position de l'arbre courant

--- scripts/three.js
+++ scripts/three.js
@@ -1,4 +1,7 @@
 const parseUrl = new URL(window.location.href);
+const currentThree = [];
+const currentThreePos = [];
+const threeEtiquette = parseUrl.searchParams.get("id");
 
 fetch(
   "https://opendata.arcgis.com/datasets/bd092c4a648b4012a28b048affa8ec1c_0.geojson"
@@ -6,14 +9,26 @@ fetch(
   .then((response) => response.json())
   .then((data) => {
     getCurrentThree(data);
+    getPositionThree(data);
   });
 
-function getCurrentThree(data) {
-  const threeEtiquette = parseUrl.searchParams.get("id");
+// recuperer le tableau de donnee de l'arbre courrant
 
+function getCurrentThree(data) {
   for (i = 0; i < data.features.length; i++) {
     if (data.features[i].properties.etiquette === threeEtiquette) {
-      console.log(data.features[i].properties);
+      currentThree.push(data.features[i].properties);
+      return currentThree;
+    }
+  }
+}
+// recuperer la position de l'arbre courant
+
+function getPositionThree(data) {
+  for (i = 0; i < data.features.length; i++) {
+    if (data.features[i].properties.etiquette === threeEtiquette) {
+      currentThreePos.push(data.features[i].geometry.coordinates);
+      return currentThreePos;
     }
   }
 }
