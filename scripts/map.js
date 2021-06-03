ebc5719b9da71188a12bad3974eeca026218adbe
Stoyann <stoyann.openclassrooms@gmail.com>
2021-06-03 19:40:48 +1100

    affichage tuiles map

--- scripts/map.js
+++ scripts/map.js
@@ -1,1 +1,15 @@
-const carte = L.map("ma-carte").setView([51.505, -0.09], 13);
+// var map = L.map("mapid").setView([51.505, -0.09], 13);
+
+// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//   attribution:
+//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// }).addTo(map);
+
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
+var mymap = L.map("mapid").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+  maxZoom: 18,
+}).addTo(mymap);
